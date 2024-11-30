93ed3612b2ae5ea4f541390da21577a0efd3ab5e
jv24601 <108680838+jv24601@users.noreply.github.com>
2024-11-30 10:39:59 -0600

    cleaner code and data

--- src/routes.py
+++ src/routes.py
@@ -5,7 +5,7 @@ from src.models import top_df
 # Create a blueprint
 home_bp = Blueprint('home', __name__, template_folder='templates')
 
-@home_bp.route('/top/<type>')
+@home_bp.route('/list/<type>')
 def test(type):
     return render_template('top.html',items=top_df[top_df['type']==type])
 
