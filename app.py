cdd73853177c559d38781031d89c01eb0cefd4cd
wright-benjamin-1701 <108680838+wright-benjamin-1701@users.noreply.github.com>
2024-11-30 18:14:21 -0600

    habe ich mich gefragt

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template
 from src.routes import home_bp
 from src.models import top_df
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__, template_folder="templates")
 app.register_blueprint(home_bp)
 
-@app.route('/')
-def home():
-    return render_template('top.html',items=top_df[top_df['type']=='lists'])
 
-if __name__ == '__main__':
+@app.route("/")
+def home():
+    return render_template("top.html", items=top_df[top_df["type"] == "lists"])
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- src/routes.py
+++ src/routes.py
@@ -3,10 +3,9 @@ from flask import Flask, Blueprint, render_template
 from src.models import top_df
 
 # Create a blueprint
-home_bp = Blueprint('home', __name__, template_folder='templates')
+home_bp = Blueprint("home", __name__, template_folder="templates")
 
-@home_bp.route('/list/<type>')
+
+@home_bp.route("/list/<type>")
 def test(type):
-    return render_template('top.html',items=top_df[top_df['type']==type])
-
-
+    return render_template("top.html", items=top_df[top_df["type"] == type])
